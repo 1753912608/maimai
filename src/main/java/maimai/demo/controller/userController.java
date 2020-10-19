7a2286d6fdd450a91f7a3b8252b2cc720d0708a8
yiluxiangbei666 <1182764581@qq.com>
2020-10-19 20:35:16 +0800

    fgz提交

--- src/main/java/maimai/demo/controller/userController.java
+++ src/main/java/maimai/demo/controller/userController.java
@@ -1,10 +1,4 @@
 package maimai.demo.controller;
 
-import org.springframework.stereotype.Controller;
-
-@Controller
 public class userController {
-    public static void main(String[] args) {
-
-    }
 }
