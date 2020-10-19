84b051f1c40ea9191486dbbcc8d6643babb39be9
yiluxiangbei666 <1182764581@qq.com>
2020-10-19 20:05:47 +0800

    细符正提交

--- src/main/java/maimai/demo/controller/exploreController.java
+++ src/main/java/maimai/demo/controller/exploreController.java
@@ -1,4 +1,5 @@
 package maimai.demo.controller;
 
 public class exploreController {
+
 }
--- src/main/java/maimai/demo/controller/userController.java
+++ src/main/java/maimai/demo/controller/userController.java
@@ -1,4 +1,10 @@
 package maimai.demo.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class userController {
+    public static void main(String[] args) {
+
+    }
 }
